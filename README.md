3d45189a095ef19df715fccaabd7758e0bec998e
Jack Kilgore <kilgorej99@gmail.com>
2021-01-19 10:28:24 -0800

    testing youtube embed

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Winter 2021 Human Computer Interaction
 
 ## Notes on Compressing
 `sips --resampleHeight 600 *.jpg`
+
+## Lazy Loading Embedded Video
+https://css-tricks.com/lazy-load-embedded-youtube-videos/
--- gplus-youtubeembed.js
+++ gplus-youtubeembed.js
@@ -10,39 +10,50 @@ function optimizeYouTubeEmbeds() {
     
     // Loop through each iframe in the page.
     for ( var i = 0; i < frames.length; i++ ) {
+        frames[i].contentWindow.document.close();
 
         // Find out youtube embed iframes.
-        if ( frames[ i ].src && frames[ i ].src.length > 0 && frames[ i ].src.match(/http(s)?:\/\/www\.youtube\.com/)) {
+        // if ( frames[ i ].src && frames[ i ].src.length > 0 && frames[ i ].src.match(/http(s)?:\/\/www\.youtube\.com/)) {
 
-            // For Youtube iframe, extract src and id.
-            var src=frames[i].src;
-            var p = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
-            var id=(src.match(p) ? RegExp.$1 : false);
-            if(id == false) { continue;}
+        //     // For Youtube iframe, extract src and id.
+        //     var src=frames[i].src;
+        //     var p = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+        //     var id=(src.match(p) ? RegExp.$1 : false);
+        //     if(id == false) { continue;}
             
-            // Get width and height.
-            var w=frames[i].width;
-            var h=frames[i].height; 
-            if(src == '' || w=='' || h=='') {continue;}
+        //     // Get width and height.
+        //     var w=frames[i].width;
+        //     var h=frames[i].height; 
+        //     if(src == '' || w=='' || h=='') {continue;}
             
-            // Thease are to position the play button centrally.
-            var pw=Math.ceil(w/2-38.5);
-            var ph=Math.ceil(h/2+38.5);    
+        //     // Thease are to position the play button centrally.
+        //     var pw=Math.ceil(w/2-38.5);
+        //     var ph=Math.ceil(h/2+38.5);    
             
-            // The image+button overlay code.
-            var code='<div style="position:relative; width: '+w+'px; height: '+h+'; overflow: hidden;margin:0 auto"><a href="#"  onclick="LoadYoutubeVidOnPreviewClick(\''+id+'\','+w+','+h+');return false;" id="skipser-youtubevid-'+id+'"><img src="http://img.youtube.com/vi/'+id+'/0.jpg" style="width:100%; height:100%; margin: -9.5% 0px -12%;" /><div style="background-image: url("workspace/iconfinder_play_alt_118620.png");height: 77px;width: 77px; position:relative; margin-left:'+pw+'px; margin-top:-'+ph+'px;z-index:5;"></div></a></div>';
-            // var code='<a href="#"  onclick="LoadYoutubeVidOnPreviewClick(\''+id+'\','+w+','+h+');return false;" id="skipser-youtubevid-'+id+'"><img src="http://i.ytimg.com/vi/'+id+'/0.jpg" style="width:'+w+'px; height:'+h+'px;" /></a>'
-            // var code = '<div style="position:relative; width: '+w+'px; height: '+h+'; overflow: hidden;"><img src="http://img.youtube.com/vi/'+id+'/0.jpg" style="width:100%; height:100%; margin: -9.5% 0px -12%;" /></div>'
-            // Replace the iframe with a the image+button code.
-            var div = document.createElement('div');
-            div.innerHTML=code;
-            div=div.firstChild;
-            frames[i].parentNode.replaceChild(div, frames[i]);
-			i--;
-        }
+        //     // The image+button overlay code.
+        //     var code='<div style="position:relative; width: '+w+'px; height: '+h+'; overflow: hidden;margin:0 auto"><a href="#"  onclick="LoadYoutubeVidOnPreviewClick(\''+id+'\','+w+','+h+');return false;" id="skipser-youtubevid-'+id+'"><div class="playbutton"><img src="http://img.youtube.com/vi/'+id+'/0.jpg" class="thumbnail"/></div></a></div>';
+        //     //<div style="background-image: url("workspace/iconfinder_play_alt_118620.png");height: 77px;width: 77px; position:relative; margin-left:'+pw+'px; margin-top:-'+ph+'px;z-index:5;"></div>
+        //     // var code = '<div style="position:relative; width: '+w+'px; height: '+h+'; overflow: hidden;"><img src="http://img.youtube.com/vi/'+id+'/0.jpg" style="width:100%; height:100%; margin: -9.5% 0px -12%;" /></div>'
+        //     // Replace the iframe with a the image+button code.
+        //     var div = document.createElement('div');
+        //     div.innerHTML=code;
+        //     div=div.firstChild;
+        //     frames[i].parentNode.replaceChild(div, frames[i]);
+		// 	i--;
+        // }
     }
 }
+
 // Replace preview image of a video with it's iframe.
+// function LoadYoutubeVidOnPreviewClick(id,w ,h) {
+//     var code='<iframe src="https://www.youtube.com/embed/'+id+'/?autoplay=1&autohide=1&border=0&wmode=opaque&enablejsapi=1" width="'+w+'" height="'+h+'" frameborder=0 allowfullscreen style="border:1px solid #ccc;" ></iframe>';
+//     var iframe = document.createElement('div');
+//     iframe.innerHTML=code;
+//     iframe=iframe.firstChild;
+//     var div=document.getElementById("skipser-youtubevid-"+id);
+//     div.parentNode.replaceChild( iframe, div)
+// }
+// // Replace preview image of a video with it's iframe.
 function LoadYoutubeVidOnPreviewClick(id,w ,h) {
     var code='<iframe src="https://www.youtube.com/embed/'+id+'/?&mute=0&showinfo=0&rel=0&autoplay=1&loop=0&control=0" width="'+w+'" height="'+h+'" frameborder=0 allowfullscreen allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" style="border:1px solid #ccc;" ></iframe>';
     var iframe = document.createElement('div');
