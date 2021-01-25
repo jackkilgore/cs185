b3287805eb397cc636eee0289c374755f32ef4ae
Jack Kilgore <kilgorej99@gmail.com>
2021-01-25 14:57:12 -0800

    finished image display

--- scripts/expand_image.js
+++ scripts/expand_image.js
@@ -1,29 +1,38 @@
 var modal = document.getElementById('Lightbox');
 var isReady = false;
-
 var click_handle;
 
-function clicker(event, img_t) {
-  if (!img_t.contains(event.target) && isReady) {
+function clicker(event) {
+  if (!event.target.closest('img.zoom') && isReady) {
     closeImage();
     return;
+  } else {
   }
   isReady = true;
 }
 
-function openImage(id) {
+function openImage(image_caller) {
+  var isMobile = navigator.userAgent.toLowerCase().match(/mobile/i)
+
+  var comp_image_path = image_caller.src;
+  var image_expand_path = comp_image_path.substr(comp_image_path.lastIndexOf('/') + 1);
+  image_expand_path = '../assets/' + image_expand_path;
+
+  // If mobile, just return a link to the uncompressed photo.
+  if (isMobile) {
+    location.href = image_expand_path;
+    return;
+  }
+
+  document.getElementById('sick').src = image_expand_path;
   document.getElementById('Lightbox').style.display = 'block';
-//   document.getElementById(id).img.src.replace();
 
   isReady = false;
-  click_handle = function clicker_handler(event) {
-    var img = document.getElementById(id);
-    clicker(event, img);
-  }
-  window.addEventListener('click', click_handle);
+  window.addEventListener('click', clicker);
 }
 
 function closeImage() {
   document.getElementById('Lightbox').style.display = 'none'
-  window.removeEventListener('click', click_handle);
+  window.removeEventListener('click', clicker);
+  isReady = false;
 }
