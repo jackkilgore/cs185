58bbdd3056818c235a4174e98771be399540491c
Jack Kilgore <kilgorej99@gmail.com>
2021-02-21 12:48:22 -0800

    Updates

---
+++ precache-manifest.05640d9b4295d12f9e366e0b47774a4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ccf4eccd403c7118789",
+    "url": "/cs185-hci/static/css/main.f80509f3.chunk.css"
+  },
+  {
+    "revision": "6ccf4eccd403c7118789",
+    "url": "/cs185-hci/static/js/main.a881df88.chunk.js"
+  },
+  {
+    "revision": "7b405f15da81619032ea",
+    "url": "/cs185-hci/static/js/runtime~main.957eb108.js"
+  },
+  {
+    "revision": "322f54e2c4a776ae2552",
+    "url": "/cs185-hci/static/js/2.6b862ed4.chunk.js"
+  },
+  {
+    "revision": "eccbd05be988f5793216",
+    "url": "/cs185-hci/static/js/3.d2f7d97f.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/cs185-hci/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "3b1c6484930f99530f4308523e41e5a4",
+    "url": "/cs185-hci/index.html"
+  }
+];
